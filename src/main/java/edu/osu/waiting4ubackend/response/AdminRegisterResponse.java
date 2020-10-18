8569dca731463119170e1e06905bf33670dd96a6
Jessie Tang <tangqiaojie@hotmail.com>
2020-10-18 12:47:48 -0700

    create AdminRegisterResponse class which defines the fields and methods of admin register response

--- src/main/java/edu/osu/waiting4ubackend/response/AdminRegisterResponse.java
+++ src/main/java/edu/osu/waiting4ubackend/response/AdminRegisterResponse.java
@@ -1,4 +1,30 @@
 package edu.osu.waiting4ubackend.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AdminRegisterResponse {
+    @JsonProperty("admin_ID")
+    private String id;
+    @JsonProperty("admin_name")
+    private String adminName;
+    private String email;
+
+    public AdminRegisterResponse(String id, String adminName, String email) {
+        this.id = id;
+        this.adminName = adminName;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
