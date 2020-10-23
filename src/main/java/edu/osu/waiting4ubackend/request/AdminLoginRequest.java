a8d63df7a328c8415b1a2f75737c13716375f354
Jessie Tang <tangqiaojie@hotmail.com>
2020-10-23 09:51:03 -0700

    add AdminLoginRequest class to handle the field and method of request

--- src/main/java/edu/osu/waiting4ubackend/request/AdminLoginRequest.java
+++ src/main/java/edu/osu/waiting4ubackend/request/AdminLoginRequest.java
@@ -1,4 +1,24 @@
 package edu.osu.waiting4ubackend.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class AdminLoginRequest {
+    private String password;
+    private String email;
+
+    public AdminLoginRequest(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
