9cafc2719bd65179599a68daaeff0acc30215954
Jessie Tang <tangqiaojie@hotmail.com>
2020-10-23 09:51:58 -0700

    add AdminLoginResponse class to handle the field and method of response

--- src/main/java/edu/osu/waiting4ubackend/response/AdminLoginResponse.java
+++ src/main/java/edu/osu/waiting4ubackend/response/AdminLoginResponse.java
@@ -1,4 +1,17 @@
 package edu.osu.waiting4ubackend.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AdminLoginResponse {
+    @JsonProperty("admin_id")
+    private String id;
+
+    public AdminLoginResponse(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
 }
