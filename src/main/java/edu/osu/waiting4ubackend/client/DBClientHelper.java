8bc6825efc71078aabdc0f0c16ebe2da8847df4f
Jessie Tang <tangqiaojie@hotmail.com>
2020-10-29 13:45:43 -0700

    modified convertToList method

--- src/main/java/edu/osu/waiting4ubackend/client/DBClientHelper.java
+++ src/main/java/edu/osu/waiting4ubackend/client/DBClientHelper.java
@@ -6,6 +6,8 @@ import com.google.cloud.datastore.Value;
 import java.util.ArrayList;
 import java.util.List;
 
+//https://stackoverflow.com/questions/48389765/google-cloud-datastore-api-how-to-add-string-array-list
+//https://developers.google.com/protocol-buffers/docs/reference/java/com/google/protobuf/StringValue
 public class DBClientHelper {
     public static List<Value<String>> convertToValueList(List<String> list) {
         if(list == null) return null;
@@ -19,8 +21,8 @@ public class DBClientHelper {
     public static List<String> convertToList(List<Value<String>> valueList) {
         if(valueList == null) return null;
         List<String> list = new ArrayList<>();
-        for(Value v : valueList) {
-            list.add(v.toString());
+        for(Value<String> v : valueList) {
+            list.add(v.get());
         }
         return list;
     }
