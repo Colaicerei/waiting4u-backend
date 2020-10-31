c1f7b70c0e5786bd6143dd55f68a616b29efcc4c
Jessie Tang <tangqiaojie@hotmail.com>
2020-10-30 21:50:08 -0700

    used the Pet class instead of PetRequest class to handle the attributes in request body

--- src/main/java/edu/osu/waiting4ubackend/controller/PetController.java
+++ src/main/java/edu/osu/waiting4ubackend/controller/PetController.java
@@ -2,36 +2,43 @@ package edu.osu.waiting4ubackend.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.Timestamp;
 import edu.osu.waiting4ubackend.client.AdminDBClient;
 import edu.osu.waiting4ubackend.client.PetDBClient;
+import edu.osu.waiting4ubackend.entity.Admin;
 import edu.osu.waiting4ubackend.entity.Pet;
 import edu.osu.waiting4ubackend.request.PetRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 public class PetController {
     @CrossOrigin
     @PostMapping(value = "/admins/{id}/pets", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<String> createPet(@PathVariable long id, @RequestBody PetRequest request) throws JsonProcessingException {
+    public ResponseEntity<String> createPet(@PathVariable long id, @RequestBody Pet petRequest) throws JsonProcessingException {
 
-        Pet pet = new Pet.PetBuilder()
-                .setPetName(request.getPetName())
-                .setDateOfBirth(request.getDateOfBirth())
-                .setType(request.getType())
-                .setBreed(request.getBreed())
-                .setDispositions(request.getDispositions())
-                .setDescription(request.getDescription())
+        Pet newPet = new Pet.PetBuilder()
+                .setPetName(petRequest.getPetName())
+                .setDateOfBirth(petRequest.getDateOfBirth())
+                .setDateCreated(new Date())
+                .setType(petRequest.getType())
+                .setBreed(petRequest.getBreed())
+                .setAvailability(petRequest.getAvailability())
+                .setStatus(petRequest.getStatus())
+                .setDescription(petRequest.getDescription())
+                .setDispositions(petRequest.getDispositions())
                 .setAdminId(String.valueOf(id))
-                .setImageUrl(request.getImageUrl())
+                .setImageUrl(petRequest.getImageUrl())
                 .build();
 
         PetDBClient petDBClient = new PetDBClient();
         //save pet into database
-        Pet petResponse = petDBClient.savePet(pet);
+        Pet petResponse = petDBClient.savePet(newPet);
         //update the pet field in admin table
         AdminDBClient adminDBClient = new AdminDBClient();
         adminDBClient.updatePetEntity(petResponse.getId(), id);
@@ -39,13 +46,53 @@ public class PetController {
         return new ResponseEntity<>(objectMapper.writeValueAsString(petResponse), HttpStatus.CREATED);
 
     }
+    /*
 
+     */
     @CrossOrigin
-    @GetMapping(value = "pets", produces = "application/json")
+    @GetMapping(value = "/pets", produces = "application/json")
     public ResponseEntity<String> getPets() throws JsonProcessingException {
         PetDBClient petDBClient = new PetDBClient();
         List<Pet> petList = petDBClient.getPets();
         ObjectMapper objectMapper = new ObjectMapper();
         return new ResponseEntity<>(objectMapper.writeValueAsString(petList), HttpStatus.OK);
     }
+    /*
+     * Method: getPetsByAdmin
+     * RequestBody: N/A
+     * PathVariable: adminId
+     * Description: A admin sent get request to retrieve pets information which are created by this admin
+     * Return: A array of Json Object
+     */
+    @CrossOrigin
+    @GetMapping(value = "/admins/{id}/pets", produces = "application/json")
+    public ResponseEntity<String> getPetsByAdmin(@PathVariable long id) throws JsonProcessingException {
+        //check valid admin id
+        AdminDBClient adminDbClient = new AdminDBClient();
+        Admin admin = adminDbClient.getAdminById(id);
+        if(admin == null) {
+            return new ResponseEntity<>("{\"Error\":  \"Unauthorized user\"}", HttpStatus.NOT_FOUND);
+        }
+        PetDBClient petDBClient = new PetDBClient();
+        List<Pet> petList = petDBClient.getPetsByAdmin(String.valueOf(id));
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new ResponseEntity<>(objectMapper.writeValueAsString(petList), HttpStatus.OK);
+    }
+
+//    @CrossOrigin
+//    @GetMapping(value = "/admins/{admin_id}/pets/{pet_id}", produces = "application/json")
+//    public ResponseEntity<String> getPet(@PathVariable long adminId, long petId) {
+//        //check valid admin id
+//        AdminDBClient adminDbClient = new AdminDBClient();
+//        Admin admin = adminDbClient.getAdminById(adminId);
+//        if(admin == null) {
+//            return new ResponseEntity<>("{\"Error\":  \"Unauthorized user\"}", HttpStatus.NOT_FOUND);
+//        }
+//        //check valid pet id which associates with admin id
+//        PetDBClient petDBClient = new PetDBClient();
+//        petDBClient.getPetByAdmin(petId, String.valueOf(adminId));
+//
+//    }
+
+
 }
