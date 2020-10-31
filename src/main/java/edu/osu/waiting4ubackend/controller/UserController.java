dbee90b6ddebcaa534aa767bef3e169f492cf3f6
Chen Zou <zouch@oregonstate.edu>
2020-10-31 11:11:38 -0700

    update user with adding preference

--- src/main/java/edu/osu/waiting4ubackend/controller/UserController.java
+++ src/main/java/edu/osu/waiting4ubackend/controller/UserController.java
@@ -16,7 +16,6 @@ import edu.osu.waiting4ubackend.request.UserUpdateRequest;
 
 import javax.validation.Valid;
 import java.io.IOException;
-import java.util.Arrays;
 
 import edu.osu.waiting4ubackend.response.UserLoginResponse;
 
